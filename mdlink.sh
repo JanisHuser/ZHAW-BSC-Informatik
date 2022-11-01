2f8d68a32f16cca518604de56538d0b84ae82c00
huserjan <28865968+JanisHuser@users.noreply.github.com>
2022-11-01 00:21:12 -0700

    create pdf file from directory

--- mdlink.sh
+++ mdlink.sh
@@ -1,10 +1,18 @@
 # /usr/bin/bash
 
+echo "====================="
+echo "searching for markdwon files in $1"
 
 OUTPUT=$(find $1 -name "*.md")
 
+echo "found the following files:"
+echo $1
+echo "====================="
+
 dt=${OUTPUT//$'\n'/' '} # Remove all newlines.
 
-echo $dt
+pdf_file_loc="$1output.pdf"
 
-exec $(pandoc $dt -o $1output.pdf)
+echo "writing pdf to $pdf_file_loc"
+
+exec $(pandoc $dt -o $pdf_file_loc)
